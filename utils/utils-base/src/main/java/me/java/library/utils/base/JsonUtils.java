10e2721aba5c361f3804d9d661bbe1147cfcce72
sylar <81623859@qq.com>
2019-11-19 09:31:03 +0800

    by sylar

--- utils/utils-base/src/main/java/me/java/library/utils/base/JsonUtils.java
+++ utils/utils-base/src/main/java/me/java/library/utils/base/JsonUtils.java
@@ -108,6 +108,61 @@ public class JsonUtils {
         return parseArray(toJSONString(source), targetClass);
     }
 
+    public static String format(String strJson) {
+        // 计数tab的个数
+        int tabNum = 0;
+        StringBuilder jsonFormat = new StringBuilder();
+        int length = strJson.length();
+
+        char last = 0;
+        for (int i = 0; i < length; i++) {
+            char c = strJson.charAt(i);
+            if (c == '{') {
+                tabNum++;
+                jsonFormat.append(c + "\n");
+                jsonFormat.append(getSpaceOrTab(tabNum));
+            } else if (c == '}') {
+                tabNum--;
+                jsonFormat.append("\n");
+                jsonFormat.append(getSpaceOrTab(tabNum));
+                jsonFormat.append(c);
+            } else if (c == ',') {
+                jsonFormat.append(c + "\n");
+                jsonFormat.append(getSpaceOrTab(tabNum));
+            } else if (c == ':') {
+                jsonFormat.append(c + " ");
+            } else if (c == '[') {
+                tabNum++;
+                char next = strJson.charAt(i + 1);
+                if (next == ']') {
+                    jsonFormat.append(c);
+                } else {
+                    jsonFormat.append(c + "\n");
+                    jsonFormat.append(getSpaceOrTab(tabNum));
+                }
+            } else if (c == ']') {
+                tabNum--;
+                if (last == '[') {
+                    jsonFormat.append(c);
+                } else {
+                    jsonFormat.append("\n" + getSpaceOrTab(tabNum) + c);
+                }
+            } else {
+                jsonFormat.append(c);
+            }
+            last = c;
+        }
+        return jsonFormat.toString();
+    }
+
+    private static String getSpaceOrTab(int tabNum) {
+        StringBuffer sbTab = new StringBuffer();
+        for (int i = 0; i < tabNum; i++) {
+            sbTab.append('\t');
+        }
+        return sbTab.toString();
+    }
+
     static class JsonValidator {
 
         private CharacterIterator it;
