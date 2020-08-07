8db3ef6b439ffed0d59909f5023b07fcc6c9964c
sylar <81623859@qq.com>
2020-08-07 18:16:01 +0800

    by sylar

--- io/io-store/io-store-mqtt/io-store-mqtt-server/src/main/java/me/java/library/io/store/mqtt/server/MqttServerPipe.java
+++ io/io-store/io-store-mqtt/io-store-mqtt-server/src/main/java/me/java/library/io/store/mqtt/server/MqttServerPipe.java
@@ -1,45 +1,45 @@
-package me.java.library.io.store.mqtt.server;
-
-import com.hivemq.embedded.EmbeddedHiveMQ;
-import com.hivemq.embedded.EmbeddedHiveMQBuilder;
-import me.java.library.io.base.cmd.Cmd;
-import me.java.library.io.base.pipe.BasePipe;
-
-import java.util.concurrent.TimeUnit;
-
-/**
- * @author : sylar
- * @fullName : me.java.library.io.store.mqtt.server.MqttServerPipe
- * @createDate : 2020/8/7
- * @description :
- * @copyRight : COPYRIGHT(c) me.iot.com All Rights Reserved
- * *******************************************************************************************
- */
-public class MqttServerPipe extends BasePipe {
-    final EmbeddedHiveMQ hiveMQ;
-
-    public MqttServerPipe() {
-        EmbeddedHiveMQBuilder builder = EmbeddedHiveMQBuilder.builder();
-        hiveMQ = builder.build();
-    }
-
-    @Override
-    protected boolean onStart() throws Exception {
-        return hiveMQ.start().isDone();
-    }
-
-    @Override
-    protected boolean onStop() throws Exception {
-        return hiveMQ.stop().isDone();
-    }
-
-    @Override
-    protected boolean onSend(Cmd request) throws Exception {
-        return false;
-    }
-
-    @Override
-    protected Cmd onSyncSend(Cmd request, long timeout, TimeUnit unit) throws Exception {
-        return null;
-    }
-}
+//package me.java.library.io.store.mqtt.server;
+//
+//import com.hivemq.embedded.EmbeddedHiveMQ;
+//import com.hivemq.embedded.EmbeddedHiveMQBuilder;
+//import me.java.library.io.base.cmd.Cmd;
+//import me.java.library.io.base.pipe.BasePipe;
+//
+//import java.util.concurrent.TimeUnit;
+//
+///**
+// * @author : sylar
+// * @fullName : me.java.library.io.store.mqtt.server.MqttServerPipe
+// * @createDate : 2020/8/7
+// * @description :
+// * @copyRight : COPYRIGHT(c) me.iot.com All Rights Reserved
+// * *******************************************************************************************
+// */
+//public class MqttServerPipe extends BasePipe {
+//    final EmbeddedHiveMQ hiveMQ;
+//
+//    public MqttServerPipe() {
+//        EmbeddedHiveMQBuilder builder = EmbeddedHiveMQBuilder.builder();
+//        hiveMQ = builder.build();
+//    }
+//
+//    @Override
+//    protected boolean onStart() throws Exception {
+//        return hiveMQ.start().isDone();
+//    }
+//
+//    @Override
+//    protected boolean onStop() throws Exception {
+//        return hiveMQ.stop().isDone();
+//    }
+//
+//    @Override
+//    protected boolean onSend(Cmd request) throws Exception {
+//        return false;
+//    }
+//
+//    @Override
+//    protected Cmd onSyncSend(Cmd request, long timeout, TimeUnit unit) throws Exception {
+//        return null;
+//    }
+//}
