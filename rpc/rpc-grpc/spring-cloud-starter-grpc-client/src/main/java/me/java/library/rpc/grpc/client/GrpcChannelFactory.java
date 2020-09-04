b2ce3458f6dc07a83bfcb1449164aec1d10ddbea
81623859@qq.com <81623859@qq.com>
2020-09-04 17:56:12 +0800

    by sylar

---
+++ rpc/rpc-grpc/spring-cloud-starter-grpc-client/src/main/java/me/java/library/rpc/grpc/client/GrpcChannelFactory.java
@@ -0,0 +1,18 @@
+package me.java.library.rpc.grpc.client;
+
+import java.util.List;
+
+import io.grpc.Channel;
+import io.grpc.ClientInterceptor;
+
+/**
+ * User: Michael
+ * Email: yidongnan@gmail.com
+ * Date: 5/17/16
+ */
+public interface GrpcChannelFactory {
+
+    Channel createChannel(String name);
+
+    Channel createChannel(String name, List<ClientInterceptor> interceptors);
+}
